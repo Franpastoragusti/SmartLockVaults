27e77749fe550427a1cd1646116fbcd266d3a005
Fran <franciscopa91@gmail.com>
2023-07-22 21:58:04 +0200

    style improvements

--- packages/nextjs/components/core/distributionCard/distributionCard.tsx
+++ packages/nextjs/components/core/distributionCard/distributionCard.tsx
@@ -1,50 +1,48 @@
-import styles from "./distributionCard.module.css"
+import styles from "./distributionCard.module.css";
 import { Address, Balance } from "~~/components/scaffold-eth";
 
 interface IProps {
-    distribution:IDistributionSelections
+  distribution: IDistributionSelections;
 }
 
-
 export interface IDistributionSelections {
-    type: "equal" | "not-equal";
-    frecuency: "each" | "periodic";
-    time: "blocks" | "days" | "years";
-    period: number;
-    distributeAddresses: any[];
-    distributionBlock: number;
-    address:string
-  }
+  type: "equal" | "not-equal";
+  frecuency: "each" | "periodic";
+  time: "blocks" | "days" | "years";
+  period: number;
+  distributeAddresses: any[];
+  distributionBlock: number;
+  address: string;
+}
 
 export const DistributionCard = ({ distribution }: IProps) => {
-    const distributionLabel = `Equatable Distribution each ${distribution.period} ${distribution.time}`
-    return (
-<li className={styles.distribution}>
-    <div className={styles.addressContainer}>
+  const distributionLabel = `Equatable Distribution each ${distribution.period} ${distribution.time}`;
+  return (
+    <li className={styles.distribution}>
+      <div className={styles.addressContainer}>
         <Address size="sm" address={distribution.address} format="short" />
         <Balance address={distribution.address} />
-    </div>
-    <div className={styles.information}>
-      <div className={styles.section}>
-        <div>
+      </div>
+      <div className={styles.information}>
+        <div className={styles.section}>
+          <div>
             <p className={styles.infoTitle}>Definition</p>
-          <p className={styles.infoLabel}>{distributionLabel}</p>
+            <p className={styles.infoLabel}>{distributionLabel}</p>
+          </div>
+          <div>
+            <p className={styles.infoTitle}>Next distribution</p>
+            <p className={styles.infoLabel}>{distribution.distributionBlock}</p>
+          </div>
         </div>
-        <div>
-          <p className={styles.infoTitle}>Next distribution</p>
-          <p className={styles.infoLabel}>{distribution.distributionBlock}</p>
-        </div>
-      </div>
-      <div className={styles.section}>
-            <p className={styles.infoTitle}>Beneficiaries</p>
-            <div className={styles.beneficiaries}>
+        <div className={styles.section}>
+          <p className={styles.infoTitle}>Beneficiaries</p>
+          <div className={styles.beneficiaries}>
             {distribution.distributeAddresses.map(addresse => (
-            <Address key={addresse} address={addresse}></Address>
-          ))}
-            </div>
-        
+              <Address key={addresse} address={addresse}></Address>
+            ))}
+          </div>
+        </div>
       </div>
-    </div>
     </li>
-)
-          }
+  );
+};
--- packages/nextjs/components/core/header/Header.tsx
+++ packages/nextjs/components/core/header/Header.tsx
@@ -45,7 +45,7 @@ export const Header = () => {
   return (
     <div className="sticky lg:static top-0 navbar bg-base-100 min-h-0 flex-shrink-0 justify-between z-20 px-0 sm:px-2">
       <div className="navbar-start w-auto lg:w-1/2">
-        <div className="lg:hidden dropdown" ref={burgerMenuRef}>
+        {/* <div className="lg:hidden dropdown" ref={burgerMenuRef}>
           <label
             tabIndex={0}
             className={`ml-1 btn btn-ghost ${isDrawerOpen ? "hover:bg-secondary" : "hover:bg-transparent"}`}
@@ -74,7 +74,7 @@ export const Header = () => {
               ></Distributton>
             </>
           )}
-        </div>
+        </div> */}
         <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6">
           <div className="flex relative w-10 h-10">
             <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
